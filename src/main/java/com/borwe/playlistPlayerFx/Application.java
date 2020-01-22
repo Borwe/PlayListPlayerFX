38bd327dbc1f5ff8641a2ecdeb8978e0d330097a
brian.orwe <brian.orwe@gmail.com>
2020-01-22 11:51:46 +0300

    Intergrated JavaFX with SpringBoot

--- src/main/java/com/borwe/playlistPlayerFx/Application.java
+++ src/main/java/com/borwe/playlistPlayerFx/Application.java
@@ -1,14 +1,35 @@
 package com.borwe.playlistPlayerFx;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
-public class Application {
+import com.borwe.playlistPlayerFx.springConfigs.MainConfig;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application{
+
+	private static ApplicationContext context;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		context= SpringApplication.run(MainConfig.class, args);
+		launch(args);
 		System.out.println("YOLO BITCHES");
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("PlayListPlayerFX - A Media Playlist player");
+		
+		//load from mainDisplay as view
+		Parent parent=FXMLLoader.load(this.getClass().getResource("/fxml/mainDisplay.fxml"));
+		Scene scene=new Scene(parent);
+		
+		primaryStage.setScene(scene);
+		
+		primaryStage.show();
+	}
 }
---
+++ src/main/java/com/borwe/playlistPlayerFx/springConfigs/MainConfig.java
@@ -0,0 +1,9 @@
+package com.borwe.playlistPlayerFx.springConfigs;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class MainConfig {
+
+}
