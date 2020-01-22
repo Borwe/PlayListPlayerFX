ec9739068a1dca65a028b1bbe474faab5106e222
brian.orwe <brian.orwe@gmail.com>
2020-01-22 10:38:25 +0300

    Updated base package name

--- src/main/java/com/borwe/playlistPlayerFx/Application.java
+++ src/main/java/com/borwe/playlistPlayerFx/Application.java
@@ -1,4 +1,4 @@
-package com.borwe.playerfx;
+package com.borwe.playlistPlayerFx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("YOLO BITCHES");
 	}
 
 }
