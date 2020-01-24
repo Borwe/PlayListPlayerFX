15073faad393be09fd80e31fc64a8a4a66047546
brian <brian@gmail>
2020-01-24 18:02:42 +0300

    Updated Exit button with action

--- src/main/java/com/borwe/playlistPlayerFx/fx/MainFXController.java
+++ src/main/java/com/borwe/playlistPlayerFx/fx/MainFXController.java
@@ -1,5 +1,19 @@
 package com.borwe.playlistPlayerFx.fx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
 public class MainFXController {
 
+	@FXML
+	MenuItem menu_exit;
+	
+	@FXML
+	public void initialize() {
+	}
+	
+	//close the all application
+	public void closeAppliction() {
+		System.exit(0);
+	}
 }
