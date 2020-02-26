e8b7eaa12b4d943d9c5db9eaaef1e41becea3b82
brian.orwe <brian.orwe@gmail.com>
2020-02-26 19:17:47 +0300

    started refactoring the accept function of FileOpening

--- src/main/java/com/borwe/playlistPlayerFx/fx/functions/FileOpenViewer.java
+++ src/main/java/com/borwe/playlistPlayerFx/fx/functions/FileOpenViewer.java
@@ -3,6 +3,7 @@ package com.borwe.playlistPlayerFx.fx.functions;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Consumer;
 
 import com.borwe.playlistPlayerFx.Application;
@@ -10,6 +11,8 @@ import com.borwe.playlistPlayerFx.Observers.DoNothingObserver;
 import com.borwe.playlistPlayerFx.data.MultiMedia;
 import com.borwe.playlistPlayerFx.data.PlayList;
 import com.borwe.playlistPlayerFx.data.Video;
+import com.borwe.playlistPlayerFx.fx.FXCompletableGenerator;
+import com.borwe.playlistPlayerFx.fx.FXRunnable;
 import com.borwe.playlistPlayerFx.springServices.FilesHandler;
 import com.borwe.playlistPlayerFx.springServices.MultiMediaService;
 import com.borwe.playlistPlayerFx.springServices.PlayListService;
@@ -19,6 +22,7 @@ import com.borwe.playlistPlayerFx.springServices.VideoService;
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.Scheduler;
+import io.reactivex.Single;
 import io.reactivex.SingleSource;
 import io.reactivex.schedulers.Schedulers;
 import javafx.application.Platform;
@@ -46,142 +50,162 @@ public class FileOpenViewer<T extends Window> implements Consumer<T>{
      *   and Multimedia section after exiting this dialog
      */
 	@Override
-	public void accept(T t) {
-		DirectoryChooser fileChooser=new DirectoryChooser();
-        //get types to be allowed into file chooser
-        var typesListObserver= Application.getApplicationContext().getBean(TypeService.class)
-            .getAlltypes().map(type->type.getType().toLowerCase());
-
-        //open file chooser and listen for a returning
-        fileChooser.setTitle("Select a folder containing files to play");
-        File file=fileChooser.showDialog(t);
-
-        //check that file exists, otherwise user didn't select any
-        if(file!=null && file.exists() && file.isDirectory()){
-            //continue here to parse files in the folder
-            var filesObserver=Application.getApplicationContext().getBean(FilesHandler.class)
-                .getFilesInDirectory(file.getAbsolutePath());
-            
-            //check if files is empty, if so stop and don't go ahead, show error message
-            if(filesObserver.isEmpty().blockingGet()) {
-            	ButtonType okButton=new ButtonType("OK");
-            	Dialog<Void> noFilesDialog=new Dialog<Void>();
-            	noFilesDialog.getDialogPane().getButtonTypes().add(okButton);
-            	noFilesDialog.setTitle("Error");
-            	noFilesDialog.getDialogPane().setHeaderText("Sorry");
-            	noFilesDialog.getDialogPane().setContentText("Please select another folder with actual files");
-            	noFilesDialog.initModality(Modality.APPLICATION_MODAL);
-            	noFilesDialog.showAndWait();
-            	accept(t);
-            	return;
-            }else {
-            	//meaning that files actually exist, then go ahead and filter only
-            	//files that have a specific extention ending that the user has
-            	//listed for support
-            	filesObserver.filter(f->{
-            		try {
-            			
-						String extention=f.getName().toLowerCase().replace(".", " ").split(" ")[1];
-						if(typesListObserver.contains(extention).blockingGet()) {
-							return true;
-						}else {
-							return false;
-						}
-            		}catch (Exception e) {
-            			e.printStackTrace();
-            			return false;
-					}
-            	})
-            	.flatMap(f->{
-            		//now map the files to MultiMedia objects, but first create 
-            		//Video objects that are linked to MultiMedia Objects
-            		//then afterwards return the MultiMedia objects
-            		MultiMedia media=Application.getApplicationContext().getBean(MultiMedia.class);
-            		Video video=Application.getApplicationContext().getBean(Video.class);
-            		media.setName(f.getName());
-
-            		
-            		//setup the video
-            		video.setMultiMedia(media);
-            		video.setLocation(f.getAbsolutePath());
-            		video.setSeek(0L);
-            		video.setTime(-1L);
-            		/*var typ=*/Application.getApplicationContext()
-            				.getBean(TypeService.class).getAlltypes()
-            				.filter(type->{
-
-            			
-            			try {
-            				
-							String extention=f.getName().toLowerCase().replace(".", " ").split(" ")[1];
-							if(type.getType().equals(extention)) {
-								return true;
-							}
-            			}catch(Exception ex) {
-            				ex.printStackTrace();
-            				return false;
-            			}
-						return false;
-            		}).toList().subscribe(typs->{
-						video.setType(typs.get(0));
-            		},error->System.err.println("ERRORTYPE: "+error.getCause()+"\n"+error.getMessage()));
-            		
-            		//now save the video
-            		
-            		Application.getApplicationContext().getBean(MultiMediaService.class)
-            			.save(video.getMultiMedia()).subscribe(System.out::println,err->{
-            				System.err.println("ERROR: "+err.getCause()+"\n"+err.getMessage());
-            			});
-            		return Application.getApplicationContext().getBean(VideoService.class)
-            				.saveVideo(video).toObservable();
-            	}).doOnError(error->System.err.println("FUCKING: "+error.getLocalizedMessage())).toList().map(vids->{
-            				
-            		//map video to playlist
-            		PlayList playlist=Application.getApplicationContext().getBean(PlayList.class);
-            		playlist.setLocation(new File(vids.get(0).getLocation()).getParentFile().getAbsolutePath());
-            		playlist.setTime(System.currentTimeMillis());
-            		System.out.println("DEVELOPING SHIT");
-            		
-            		List<MultiMedia> medias=new ArrayList<MultiMedia>();
-            		playlist.setMarkMultiMediaAccess(medias);
-            		
-            		vids.forEach(v->{
-            			playlist.getMarkMultiMediaAccess().add(v.getMultiMedia());
-            		});
-            		return playlist;
-            	}).doOnError(error->System.err.println("SHITTYERROR: "+error.getLocalizedMessage()))
-            	.observeOn(Schedulers.io()).subscribe(playlist->{
-            		//prompt user to give playlist a name if playlist not null
-            		if(playlist!=null) {
-            			Platform.runLater(()->{
-							TextInputDialog dialog=new TextInputDialog();
-							dialog.setHeaderText("Input");
-							dialog.setContentText("Enter name of the playlist");
-							dialog.initModality(Modality.APPLICATION_MODAL);
-							dialog.showAndWait();
-							
-							String result=dialog.getResult();
-							//check that it's not null, and that it exists, then set the value of playlist 
-							//and save it asyncronously
-							if(result!=null && result.isEmpty()==false) {
-								Application.getApplicationContext().getBean(PlayListService.class)
-									.savePlayList(playlist).subscribeOn(Schedulers.io()).subscribe(pl->{
-										System.out.println("DONE SAVING PLAY LIST");
-									});
-							}
-            			});
-            		}else {
-            			//some shit went wrong
-            			System.out.println("WHAT THE FUCK? COME ON!!!!!!!!!!!!!");
-            		}
-            	},error->{
-            		System.err.println("ERROR: "+error.getCause());
-            		System.err.println("ERROR SUBSCRIBE: "+error.getLocalizedMessage());
-            	});
-            }
-        }else{
-            //meaning user didn't select anything here
-            System.out.println("Wow, you didn't select shit!'");
-        }
+	public void accept(T window) {
+		
+		getFolderSelected(window).subscribeOn(Schedulers.computation()).subscribe();
+//		DirectoryChooser fileChooser=new DirectoryChooser();
+//        //get types to be allowed into file chooser
+//        var typesListObserver= Application.getApplicationContext().getBean(TypeService.class)
+//            .getAlltypes().map(type->type.getType().toLowerCase());
+//
+//        //open file chooser and listen for a returning
+//        fileChooser.setTitle("Select a folder containing files to play");
+//        File file=fileChooser.showDialog(window);
+//
+//        //check that file exists, otherwise user didn't select any
+//        if(file!=null && file.exists() && file.isDirectory()){
+//            //continue here to parse files in the folder
+//            var filesObserver=Application.getApplicationContext().getBean(FilesHandler.class)
+//                .getFilesInDirectory(file.getAbsolutePath());
+//            
+//            //check if files is empty, if so stop and don't go ahead, show error message
+//            if(filesObserver.isEmpty().blockingGet()) {
+//            	ButtonType okButton=new ButtonType("OK");
+//            	Dialog<Void> noFilesDialog=new Dialog<Void>();
+//            	noFilesDialog.getDialogPane().getButtonTypes().add(okButton);
+//            	noFilesDialog.setTitle("Error");
+//            	noFilesDialog.getDialogPane().setHeaderText("Sorry");
+//            	noFilesDialog.getDialogPane().setContentText("Please select another folder with actual files");
+//            	noFilesDialog.initModality(Modality.APPLICATION_MODAL);
+//            	noFilesDialog.showAndWait();
+//            	accept(window);
+//            	return;
+//            }else {
+//            	//meaning that files actually exist, then go ahead and filter only
+//            	//files that have a specific extention ending that the user has
+//            	//listed for support
+//            	filesObserver.filter(f->{
+//            		try {
+//            			
+//						String extention=f.getName().toLowerCase().replace(".", " ").split(" ")[1];
+//						if(typesListObserver.contains(extention).blockingGet()) {
+//							return true;
+//						}else {
+//							return false;
+//						}
+//            		}catch (Exception e) {
+//            			e.printStackTrace();
+//            			return false;
+//					}
+//            	})
+//            	.flatMap(f->{
+//            		//now map the files to MultiMedia objects, but first create 
+//            		//Video objects that are linked to MultiMedia Objects
+//            		//then afterwards return the MultiMedia objects
+//            		MultiMedia media=Application.getApplicationContext().getBean(MultiMedia.class);
+//            		Video video=Application.getApplicationContext().getBean(Video.class);
+//            		media.setName(f.getName());
+//
+//            		
+//            		//setup the video
+//            		video.setMultiMedia(media);
+//            		video.setLocation(f.getAbsolutePath());
+//            		video.setSeek(0L);
+//            		video.setTime(-1L);
+//            		/*var typ=*/Application.getApplicationContext()
+//            				.getBean(TypeService.class).getAlltypes()
+//            				.filter(type->{
+//
+//            			
+//            			try {
+//            				
+//							String extention=f.getName().toLowerCase().replace(".", " ").split(" ")[1];
+//							if(type.getType().equals(extention)) {
+//								return true;
+//							}
+//            			}catch(Exception ex) {
+//            				ex.printStackTrace();
+//            				return false;
+//            			}
+//						return false;
+//            		}).toList().subscribe(typs->{
+//						video.setType(typs.get(0));
+//            		},error->System.err.println("ERRORTYPE: "+error.getCause()+"\n"+error.getMessage()));
+//            		
+//            		//now save the video
+//            		
+//            		Application.getApplicationContext().getBean(MultiMediaService.class)
+//            			.save(video.getMultiMedia()).subscribe(System.out::println,err->{
+//            				System.err.println("ERROR: "+err.getCause()+"\n"+err.getMessage());
+//            			});
+//            		return Application.getApplicationContext().getBean(VideoService.class)
+//            				.saveVideo(video).toObservable();
+//            	}).doOnError(error->System.err.println("FUCKING: "+error.getLocalizedMessage())).toList().map(vids->{
+//            				
+//            		//map video to playlist
+//            		PlayList playlist=Application.getApplicationContext().getBean(PlayList.class);
+//            		playlist.setLocation(new File(vids.get(0).getLocation()).getParentFile().getAbsolutePath());
+//            		playlist.setTime(System.currentTimeMillis());
+//            		System.out.println("DEVELOPING SHIT");
+//            		
+//            		List<MultiMedia> medias=new ArrayList<MultiMedia>();
+//            		playlist.setMarkMultiMediaAccess(medias);
+//            		
+//            		vids.forEach(v->{
+//            			playlist.getMarkMultiMediaAccess().add(v.getMultiMedia());
+//            		});
+//            		return playlist;
+//            	}).doOnError(error->System.err.println("SHITTYERROR: "+error.getLocalizedMessage()))
+//            	.observeOn(Schedulers.io()).subscribe(playlist->{
+//            		//prompt user to give playlist a name if playlist not null
+//            		if(playlist!=null) {
+//            			Platform.runLater(()->{
+//							TextInputDialog dialog=new TextInputDialog();
+//							dialog.setHeaderText("Input");
+//							dialog.setContentText("Enter name of the playlist");
+//							dialog.initModality(Modality.APPLICATION_MODAL);
+//							dialog.showAndWait();
+//							
+//							String result=dialog.getResult();
+//							//check that it's not null, and that it exists, then set the value of playlist 
+//							//and save it asyncronously
+//							if(result!=null && result.isEmpty()==false) {
+//								Application.getApplicationContext().getBean(PlayListService.class)
+//									.savePlayList(playlist).subscribeOn(Schedulers.io()).subscribe(pl->{
+//										System.out.println("DONE SAVING PLAY LIST");
+//									});
+//							}
+//            			});
+//            		}else {
+//            			//some shit went wrong
+//            			System.out.println("WHAT THE FUCK? COME ON!!!!!!!!!!!!!");
+//            		}
+//            	},error->{
+//            		System.err.println("ERROR: "+error.getCause());
+//            		System.err.println("ERROR SUBSCRIBE: "+error.getLocalizedMessage());
+//            	});
+//            }
+//        }else{
+//            //meaning user didn't select anything here
+//            System.out.println("Wow, you didn't select shit!'");
+//        }
     }
+	
+	private Single<File> getFolderSelected(T window){
+		return Single.defer(()->Single.just(window)
+			.map(w->{
+				ReentrantLock lock=new ReentrantLock(true);
+				
+				DirectoryChooser fileChooser=new DirectoryChooser();
+				FXRunnable showFileUI=()->{
+				fileChooser.setTitle("Select a folder containing files to play");
+				fileChooser.showDialog(window);
+				};
+				FXCompletableGenerator.doOnUI(showFileUI, lock);
+				
+				//open file chooser and listen for a returning
+				return new File("shit.txt");
+			}
+		));
+	}
 }
